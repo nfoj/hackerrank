6f38055d8d28a672975eeb844feddb0b66315fa9
Nilton Junior <nfoj_@hotmail.com>
2024-11-13 17:19:24 -0300

    Linux update

---
+++ Linux-Shell/Uniq-command-#1/solution.sh
@@ -0,0 +1,25 @@
+# In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+
+# Given a text file, remove the consecutive repetitions of any line.
+
+
+# Input
+# 00
+# 00
+# 01
+# 01
+# 00
+# 00
+# 02
+# 02
+
+
+# Output
+# 00
+# 01
+# 00
+# 02
+
+
+# Solution
+uniq
